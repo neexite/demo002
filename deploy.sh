469e519276dbd4e31737066c0d15c6137e89627c
Taras Nakonechnyi <neexite@gmail.com>
2022-06-15 08:26:15 +0300

    try to use yarn install script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 # ----------------------
 # KUDU Deployment Script
-# Version: 1.0.17
 # ----------------------
+echo "Starting the KUDU deployment script"
 
 # Helpers
 # -------
@@ -31,6 +31,10 @@ SCRIPT_DIR="${SCRIPT_DIR%/*}"
 ARTIFACTS=$SCRIPT_DIR/../artifacts
 KUDU_SYNC_CMD=${KUDU_SYNC_CMD//\"}
 
+DEPLOYMENT_TOOLS=/home/site/deployments/tools
+YARN_DIR=$DEPLOYMENT_TOOLS/yarn
+TMP_DIR=/tmp
+
 if [[ ! -n "$DEPLOYMENT_SOURCE" ]]; then
   DEPLOYMENT_SOURCE=$SCRIPT_DIR
 fi
@@ -53,7 +57,7 @@ if [[ ! -n "$KUDU_SYNC_CMD" ]]; then
   # Install kudu sync
   echo Installing Kudu Sync
   npm install kudusync -g --silent
-  exitWithMessageOnError "npm failed"
+  exitWithMessageOnError "Installing Kudu-Sync with npm failed"
 
   if [[ ! -n "$KUDU_SERVICE" ]]; then
     # In case we are running locally this is the correct location of kuduSync
@@ -62,6 +66,33 @@ if [[ ! -n "$KUDU_SYNC_CMD" ]]; then
     # In case we are running on kudu service this is the correct location of kuduSync
     KUDU_SYNC_CMD=$APPDATA/npm/node_modules/kuduSync/bin/kuduSync
   fi
+fi
+
+# Install Yarn
+# ------------
+
+echo "Installing yarn"
+if [[ ! -e "$YARN_DIR" ]]; then
+  cd "$TMP_DIR"
+  eval wget https://github.com/yarnpkg/yarn/releases/download/v1.16.0/yarn-v1.16.0.tar.gz -O yarn.tar.gz
+  exitWithMessageOnError "Downloading Yarn failed"
+  
+  eval tar -xzf yarn.tar.gz -C "$DEPLOYMENT_TOOLS"
+  exitWithMessageOnError "Extracting archive failed"
+  cd - > /dev/null
+
+  cd "$DEPLOYMENT_TOOLS"
+  eval ln -s yarn-v1.16.0/bin/yarn yarn
+  exitWithMessageOnError "Symlink failed"
+  cd - > /dev/null
+fi
+
+YARN_VERSION="$(yarn -v)"
+
+if [[ -e "$DEPLOYMENT_TOOLS/yarn" ]]; then
+  echo "Found Yarn v$YARN_VERSION"
+else
+  exitWithMessageOnError "Couldn't find Yarn"
 fi
 
 # Node Helpers
@@ -78,7 +109,7 @@ selectNodeVersion () {
       exitWithMessageOnError "getting node version failed"
     fi
     
-    if [[ -e "$DEPLOYMENT_TEMP/__npmVersion.tmp" ]]; then
+    if [[ -e "$DEPLOYMENT_TEMP/.tmp" ]]; then
       NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
       exitWithMessageOnError "getting npm version failed"
     fi
@@ -87,9 +118,9 @@ selectNodeVersion () {
       NODE_EXE=node
     fi
 
-    NPM_CMD="\"$NODE_EXE\" \"$NPM_JS_PATH\""
+    NPM_CMD=yarn
   else
-    NPM_CMD=npm
+    NPM_CMD=yarn
     NODE_EXE=node
   fi
 }
@@ -98,25 +129,50 @@ selectNodeVersion () {
 # Deployment
 # ----------
 
-echo Handling node.js deployment.
+echo "Handling node.js deployment, running on $DEPLOYMENT_SOURCE/deploy.sh"
 
-# 1. KuduSync
-if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
-  "$KUDU_SYNC_CMD" -v 50 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
-  exitWithMessageOnError "Kudu Sync failed"
-fi
-
-# 2. Select node version
+# 1. Select NodeJs version 
+echo "1. Select NodeJs Version."
 selectNodeVersion
+echo "Use $NPM_CMD to resolve packages"
 
-# 3. Install npm packages
-if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  echo "Running $NPM_CMD install --production"
-  eval $NPM_CMD install --production
-  exitWithMessageOnError "npm failed"
+# 2. Install Packages using Yarn
+echo "2. Install $NPM_CMD packages"
+if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
+  cd "$DEPLOYMENT_SOURCE"
+  eval "$NPM_CMD" install --network-timeout 1000000 --ignore-engines
+  exitWithMessageOnError "Installing npm packages failed"
+  echo "Finished installing npm packages"
   cd - > /dev/null
 fi
 
-##################################################################################################################################
+# 3. Build React App
+echo "3. Build React App"
+if [ -e "$DEPLOYMENT_SOURCE/node_modules" ]; then 
+  cd "$DEPLOYMENT_SOURCE"
+  eval "$NPM_CMD" build
+  exitWithMessageOnError "Build failed!"
+  echo "Build finished"
+  cd - > /dev/null
+fi
+
+# 4. KuduSync
+echo "4. KuduSync to '$DEPLOYMENT_TARGET'"
+if [ -e "$DEPLOYMENT_SOURCE/build" ]; then
+ cd "$DEPLOYMENT_SOURCE"
+ eval "$KUDU_SYNC_CMD" -v 100 -f "$DEPLOYMENT_SOURCE/build" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH"
+ exitWithMessageOnError "Kudu Sync failed!"
+ cd - > /dev/null
+fi
+
+# ##################################################################################################################################
+
+# Post deployment stub
+if [[ -n "$POST_DEPLOYMENT_ACTION" ]]; then
+  POST_DEPLOYMENT_ACTION=${POST_DEPLOYMENT_ACTION//\"}
+  cd "${POST_DEPLOYMENT_ACTION_DIR%\\*}"
+  "$POST_DEPLOYMENT_ACTION"
+   exitWithMessageOnError "post deployment action failed"
+fi
+
 echo "Finished successfully."
